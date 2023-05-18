21ad274265a68211872099b739a3ce4d4f8e7f06
Fahad-Rafiq06 <73123765+Fahad-Rafiq06@users.noreply.github.com>
2023-05-18 23:39:04 +0500

    Update app.js
    Functionality completed with edit button prompt

--- app.js
+++ app.js
@@ -5,13 +5,51 @@ function addtodo(){
 
     var todoItem = document.getElementById("todo-item");
 
+    // created list element
     var li = document.createElement('li');
 
     var liText = document.createTextNode(todoItem.value);
-
+    
     li.appendChild(liText);
+
+    // created delete button
+    var dltBtn = document.createElement('button');
+    
+    var dltText = document.createTextNode("Delete");
+
+    dltBtn.appendChild(dltText);
+
+    dltBtn.setAttribute('onclick', 'deleteitem(this)')
+
+    li.appendChild(dltBtn)
+
+    // create Edit Button
+    var editBtn = document.createElement("button");
+
+    var editText = document.createTextNode("Edit");
+
+    editBtn.appendChild(editText);
+
+    editBtn.setAttribute('onclick', 'edititem(this)')
+
+    li.appendChild(editBtn)
 
     list.appendChild(li);
 
     todoItem.value = ""
+}
+
+function deleteitem(e){
+    e.parentNode.remove();
+}
+
+function edititem(e){
+    var edit = prompt("Enter the updated text", e.parentNode.firstChild.nodeValue)
+    
+    e.parentNode.firstChild.nodeValue = edit;
+    // console.log(e.parentNode.firstChild.nodeValue)
+}
+
+function dltall(){
+    list.innerHTML = ""
 }
